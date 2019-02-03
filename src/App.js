00fc50054712df71b87cabcf4977a540f6b26e7d
Viktor Wällstedt <viktor.wallstedt@gmail.com>
2019-02-03 17:09:59 +0100

    Changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Yolo</p>
+          <p>Yoloswag</p>
           <a
             className="App-link"
             href="https://reactjs.org"
